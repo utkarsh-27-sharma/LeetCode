f4ea0857c23003c3625eac4ec3fdde044ea6d644
Utkarsh Sharma <65302150+utkarsh-27-sharma@users.noreply.github.com>
2022-02-13 17:14:34 +0530

    Time: 0 ms (100.00%), Space: 16 MB (18.06%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    // it is use to store answer
-    vector<vector<int>> ans;
+    vector<vector<int>> ans; // to store answer
     
-    // solve function for generating answer
-    void solve(int i, vector<int>&arr, vector<int>&temp)
+    void solve(vector<int> &arr, int i, vector<int> temp)
     {
-        if(i == arr.size()) // if index reaches the size of the array 
+        if(i == arr.size()) // if we find out one possibility
         {
-            //if we find out a possibility, push it into the answer array
-            ans.push_back(temp);
+            //push into the final answer that possibility
+            ans.push_back(temp); 
             return;
         }
         
-        // taking ith element
-        temp.push_back(arr[i]);
+        solve(arr, i + 1, temp); // finding possibility for next index
         
-        // call for the next index
-        solve(i + 1, arr, temp);
+        temp.push_back(arr[i]); // push into the array
         
-        // not taking ith ele,so first pop that already pushed ele, and then call for next index and generating the next possibility
-        temp.pop_back();
-        
-        // calling for next index
-        solve(i + 1, arr, temp);
+        solve(arr, i + 1, temp); //again finding out the next possibility
         
     }
     vector<vector<int>> subsets(vector<int>& arr) {
-        ans.clear(); // clear global array
+         vector<int> temp; // vector to store temporary answer
         
-        vector<int> temp; // temporary array for generating answer
-        solve(0, arr, temp); // generating all possibility
+        solve(arr, 0, temp); // function that will generate our final answer
         
-        return ans;
+        return ans; // return the final answer
     }
 };
